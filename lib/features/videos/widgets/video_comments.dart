ad7f764c6e6fe04cb2d48f9cd009e0025f015e99
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-01-25 17:00:34 +0900

    Comment bottom sheet

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -35,10 +36,82 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text("Im comment"),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                foregroundImage: NetworkImage(
+                    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.chosun.com%2Fsitedata%2Fimage%2F201912%2F11%2F2019121101152_0.png&f=1&nofb=1&ipt=9df9a3fcac217fdfe6a6b451562e12db8fe4600ab725e19fb7948a116e95be12&ipo=images"),
+                child: Text("백예린2"),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "yerin_the_genuine",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                      "'New Year' 잘 듣고 계신가요? 지금 블루바이닐 네이버포스트에서 앨범의 비하인드도 만나보실 수 있습니다!",
+                    ),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v3,
+                  Text(
+                    "52.2K",
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size8,
+              horizontal: Sizes.size12,
+            ),
+            child: Row(
+              children: const [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  foregroundImage: NetworkImage(
+                      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nemopan.com%2Ffiles%2Fattach%2Fimages%2F6294%2F480%2F473%2F012%2Fe6fdf0be95a17f41126453c1a06b62fd.jpg&f=1&nofb=1&ipt=3787e4fb142c7940f5592b642d0eef37e5eb6113200a999ec88c507baaeed7a7&ipo=images"),
+                  child: Text("신세경"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
