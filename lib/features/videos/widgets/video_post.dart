73f2bcfa5a32950debc472d72cfda14077dbd8dd
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-01-25 13:13:51 +0900

    video button, text, see more button

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/videos/widgets/video_button.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final void Function() onVideoFinished;
   final int index;
+  final String description =
+      "#chyonee #tiktok #clonecoding #flutter #flutterisawesome #iwanttobecomeflutterdeveloper";
 
   const VideoPost({
     super.key,
@@ -25,6 +29,7 @@ class _VideoPostState extends State<VideoPost>
       VideoPlayerController.asset("assets/videos/video.mp4");
   late final AnimationController _animationController;
   bool _isPaused = false;
+  bool _isAllDesc = false;
   final Duration _animationDuration = const Duration(milliseconds: 200);
 
   // ! 비디오 컨트롤러의 비디오 전체 길이가 현재 사용자의 비디오 실행기간이 같다 즉, 비디오가 다 끝났다면 props로 받은 onVideoFinished()를 실행
@@ -40,6 +45,7 @@ class _VideoPostState extends State<VideoPost>
   // ! 비디오 컨트롤러를 초기화하고, 초기화가 끝나면 바로 자동재생을 실행하는 코드, 그리고 이벤트 리스너를 초기화
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -85,6 +91,12 @@ class _VideoPostState extends State<VideoPost>
     });
   }
 
+  void _seeAllDescriptionTap() {
+    setState(() {
+      _isAllDesc = !_isAllDesc;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return VisibilityDetector(
@@ -126,6 +138,99 @@ class _VideoPostState extends State<VideoPost>
                   ),
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "@chyonee",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size16,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      size: Sizes.size16,
+                      color: Colors.teal.shade200,
+                    ),
+                  ],
+                ),
+                Gaps.v10,
+                Container(
+                  width: MediaQuery.of(context).size.width - 160,
+                  decoration: const BoxDecoration(),
+                  child: widget.description.length > 25 && !_isAllDesc
+                      ? Row(
+                          children: [
+                            Text(
+                              widget.description.substring(0, 10),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: Sizes.size14,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Gaps.h10,
+                            const FaIcon(
+                              FontAwesomeIcons.ellipsis,
+                              color: Colors.white,
+                              size: Sizes.size12,
+                            ),
+                            Gaps.h10,
+                            GestureDetector(
+                              onTap: _seeAllDescriptionTap,
+                              child: const Text(
+                                "See more",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: Sizes.size14,
+                                  fontWeight: FontWeight.w800,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      : Text(
+                          widget.description,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizes.size14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: Column(
+              children: const [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  foregroundImage: NetworkImage(
+                      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fichef.bbci.co.uk%2Fnews%2F640%2Fcpsprodpb%2F7036%2Fproduction%2F_111162782__313.jpg&f=1&nofb=1&ipt=19a400e48009148eee6558a4fa3c6b795dd962030b00bd5be0f646d4d5465fa1&ipo=images"),
+                  child: Text("백예린"),
+                ),
+                Gaps.v20,
+                VideoButton(icon: FontAwesomeIcons.solidHeart, text: "2.9M"),
+                Gaps.v20,
+                VideoButton(icon: FontAwesomeIcons.solidComment, text: "33K"),
+                Gaps.v20,
+                VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
+              ],
             ),
           ),
         ],
