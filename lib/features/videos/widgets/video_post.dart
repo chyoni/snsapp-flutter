763b45ea488723707c48af5eb216e7e39fca3a8b
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-02-05 18:41:47 +0900

    kIsWeb

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
@@ -47,8 +48,12 @@ class _VideoPostState extends State<VideoPost>
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
     await _videoPlayerController.setLooping(true);
-    setState(() {});
+    // ! 만약, Web이라면 소리를 0으로 한 상태로 autoplay를 하겠다는 의미
+    if (kIsWeb) {
+      await _videoPlayerController.setVolume(0);
+    }
     _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
   @override
